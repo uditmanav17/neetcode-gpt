bc502cfc2b017551c3c61e4f5975be21050178a2
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-04-18 12:53:38 +0530

    Update PyTorch basics (PyTorch 1/4)

--- foundations/pytorch_basics.py
+++ foundations/pytorch_basics.py
@@ -2,31 +2,26 @@ import torch
 import torch.nn
 from torchtyping import TensorType
 
-# Helpful functions:
-# https://pytorch.org/docs/stable/generated/torch.reshape.html
-# https://pytorch.org/docs/stable/generated/torch.mean.html
-# https://pytorch.org/docs/stable/generated/torch.cat.html
-# https://pytorch.org/docs/stable/generated/torch.nn.functional.mse_loss.html
-
-# Round your answers to 4 decimal places using torch.round(input_tensor, decimals = 4)
+# Round all answers to 4 decimal places: torch.round(tensor, decimals=4)
 class Solution:
     def reshape(self, to_reshape: TensorType[float]) -> TensorType[float]:
-        # torch.reshape() will be useful - check out the documentation
+        # Reshape (M, N) tensor to (M*N/2, 2)
+        # Use torch.reshape(tensor, new_shape)
         r, c = to_reshape.shape
-        t = torch.reshape(to_reshape, (r, c // 2, 2))
-        return t
+        new_shape = (r * c // 2, 2)
+        return to_reshape.reshape(new_shape)
 
     def average(self, to_avg: TensorType[float]) -> TensorType[float]:
-        # torch.mean() will be useful - check out the documentation
-        return torch.round(torch.mean(to_avg, axis=0), decimals = 4)
-
+        # Compute column-wise mean (average across rows)
+        # Use torch.mean(tensor, dim=0)
+        return torch.mean(to_avg, dim=0)
 
     def concatenate(self, cat_one: TensorType[float], cat_two: TensorType[float]) -> TensorType[float]:
-        # torch.cat() will be useful - check out the documentation
-        return torch.cat([cat_one, cat_two], axis=1)
-        pass
+        # Join two tensors side-by-side along dim=1
+        # Use torch.cat((a, b), dim=1)
+        return torch.cat((cat_one, cat_two), dim=1)
 
     def get_loss(self, prediction: TensorType[float], target: TensorType[float]) -> TensorType[float]:
-        # torch.nn.functional.mse_loss() will be useful - check out the documentation
+        # Compute Mean Squared Error between prediction and target
+        # Use torch.nn.functional.mse_loss(prediction, target)
         return torch.nn.functional.mse_loss(prediction, target)
-        pass
